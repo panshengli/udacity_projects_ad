e7c6891b94013935bf0f6512f5b09f848fc4de91
潘胜利 <panshengli@csvw.com>
2020-05-04 20:34:46 +0800

    added objection detection

---
+++ objection_detection/draw_bboxes.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+image = mpimg.imread('./bbox-example-image.jpg')
+
+# Define a function that takes an image, a list of bounding boxes, 
+# and optional color tuple and line thickness as inputs
+# then draws boxes in that color on the output
+
+def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
+    # make a copy of the image
+    draw_img = np.copy(img)
+    # draw each bounding box on your image copy using cv2.rectangle()
+    for bbox in bboxes:
+        # Draw a rectangle given bbox coordinates
+        cv2.rectangle(draw_img, bbox[0], bbox[1], color, thick)
+    # return the image copy with boxes drawn
+    return draw_img # Change this line to return image copy with boxes
+# Add bounding boxes in this format, these are just example coordinates.
+bboxes = [((100, 100), (200, 200)), ((300, 300), (400, 400))]
+
+result = draw_boxes(image, bboxes)
+plt.imshow(result)
